e47f62e2d58a4aa616b1b855a1d9f35d0c7f24f6
Villy Rosa <villyrosa@hotmail.com>
2022-04-05 15:44:30 -0300

    Criação da parte visual

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var Casa1 = document.querySelector("div#casa-1")
+var Casa2 = document.querySelector("div#casa-2")
+var Casa3 = document.querySelector("div#casa-3")
+var Casa4 = document.querySelector("div#casa-4")
+var Casa5 = document.querySelector("div#casa-5")
+var Casa6 = document.querySelector("div#casa-6")
+var Casa7 = document.querySelector("div#casa-7")
+var Casa8 = document.querySelector("div#casa-8")
+var Casa9 = document.querySelector("div#casa-9")
+
+// Vez = 0 é vez do X, Vez = 1 é vez da O
+var vez = 0;
+
+function ClickCasa1(){
+    alert("teste")
+    Casa1.style.backgroundColor = "red"
+}
